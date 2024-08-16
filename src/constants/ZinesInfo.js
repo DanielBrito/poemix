936f413f3c9e0caab95ff4c24fd1bdb3239da8af
Daniel Brito <daniell-henrique@hotmail.com>
2024-08-16 20:22:43 +0000

    feat: add paths and zines info

---
+++ src/constants/ZinesInfo.js
@@ -0,0 +1,308 @@
+import protestizando1Thumb from "../assets/images/zines/protestizando_1.webp"
+import protestizando2Thumb from "../assets/images/zines/protestizando_2.webp"
+import protestizando3Thumb from "../assets/images/zines/protestizando_3.webp"
+import protestizando4Thumb from "../assets/images/zines/protestizando_4.webp"
+import protestizando5Thumb from "../assets/images/zines/protestizando_5.webp"
+import protestizando6Thumb from "../assets/images/zines/protestizando_6.webp"
+import protestizando7Thumb from "../assets/images/zines/protestizando_7.webp"
+import protestizando8Thumb from "../assets/images/zines/protestizando_8.webp"
+import protestizando9Thumb from "../assets/images/zines/protestizando_9.webp"
+import protestizando10Thumb from "../assets/images/zines/protestizando_10.webp"
+import protestizando11Thumb from "../assets/images/zines/protestizando_11.webp"
+import protestizando12Thumb from "../assets/images/zines/protestizando_12.webp"
+import protestizando13Thumb from "../assets/images/zines/protestizando_13.webp"
+import protestizando14Thumb from "../assets/images/zines/protestizando_14.webp"
+import protestizando15Thumb from "../assets/images/zines/protestizando_15.webp"
+import protestizando16Thumb from "../assets/images/zines/protestizando_16.webp"
+import protestizando17Thumb from "../assets/images/zines/protestizando_17.webp"
+import protestizando18Thumb from "../assets/images/zines/protestizando_18.webp"
+import protestizando19Thumb from "../assets/images/zines/protestizando_19.webp"
+import protestizando20Thumb from "../assets/images/zines/protestizando_20.webp"
+import protestizando21Thumb from "../assets/images/zines/protestizando_21.webp"
+import protestizando22Thumb from "../assets/images/zines/protestizando_22.webp"
+import protestizando23Thumb from "../assets/images/zines/protestizando_23.webp"
+import protestizando24Thumb from "../assets/images/zines/protestizando_24.webp"
+import protestizando25Thumb from "../assets/images/zines/protestizando_25.webp"
+import protestizando26Thumb from "../assets/images/zines/protestizando_26.webp"
+import protestizando27Thumb from "../assets/images/zines/protestizando_27.webp"
+import protestizando28Thumb from "../assets/images/zines/protestizando_28.webp"
+import protestizando29Thumb from "../assets/images/zines/protestizando_29.webp"
+import protestizando30Thumb from "../assets/images/zines/protestizando_30.webp"
+import protestizando31Thumb from "../assets/images/zines/protestizando_31.webp"
+import protestizando32Thumb from "../assets/images/zines/protestizando_32.webp"
+import protestizando33Thumb from "../assets/images/zines/protestizando_33.webp"
+import protestizando34Thumb from "../assets/images/zines/protestizando_34.webp"
+import protestizando35Thumb from "../assets/images/zines/protestizando_35.webp"
+import protestizando36Thumb from "../assets/images/zines/protestizando_36.webp"
+import protestizando37Thumb from "../assets/images/zines/protestizando_37.webp"
+
+import { 
+    APP_BASE_URL,
+    ZINE_PATH,
+    PROTESTIZANDO_1_PATH,
+    PROTESTIZANDO_2_PATH,
+    PROTESTIZANDO_3_PATH,
+    PROTESTIZANDO_4_PATH,
+    PROTESTIZANDO_5_PATH,
+    PROTESTIZANDO_6_PATH,
+    PROTESTIZANDO_7_PATH,
+    PROTESTIZANDO_8_PATH,
+    PROTESTIZANDO_9_PATH,
+    PROTESTIZANDO_10_PATH,
+    PROTESTIZANDO_11_PATH,
+    PROTESTIZANDO_12_PATH,
+    PROTESTIZANDO_13_PATH,
+    PROTESTIZANDO_14_PATH,
+    PROTESTIZANDO_15_PATH,
+    PROTESTIZANDO_16_PATH,
+    PROTESTIZANDO_17_PATH,
+    PROTESTIZANDO_18_PATH,
+    PROTESTIZANDO_19_PATH,
+    PROTESTIZANDO_20_PATH,
+    PROTESTIZANDO_21_PATH,
+    PROTESTIZANDO_22_PATH,
+    PROTESTIZANDO_23_PATH,
+    PROTESTIZANDO_24_PATH,
+    PROTESTIZANDO_25_PATH,
+    PROTESTIZANDO_26_PATH,
+    PROTESTIZANDO_27_PATH,
+    PROTESTIZANDO_28_PATH,
+    PROTESTIZANDO_29_PATH,
+    PROTESTIZANDO_30_PATH,
+    PROTESTIZANDO_31_PATH,
+    PROTESTIZANDO_32_PATH,
+    PROTESTIZANDO_33_PATH,
+    PROTESTIZANDO_34_PATH,
+    PROTESTIZANDO_35_PATH,
+    PROTESTIZANDO_36_PATH,
+    PROTESTIZANDO_37_PATH,
+} from "./Routes";
+
+const zines = [
+    {
+        name: "PROTESTIZANDO #1",
+        releasedAt: "26/02/2014",
+        thumb: protestizando1Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_1_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #2",
+        releasedAt: "30/05/2014",
+        thumb: protestizando2Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_2_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #3",
+        releasedAt: "27/09/2014",
+        thumb: protestizando3Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_3_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #4",
+        releasedAt: "25/01/2015",
+        thumb: protestizando4Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_4_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #5",
+        releasedAt: "14/06/2015",
+        thumb: protestizando5Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_5_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #6",
+        releasedAt: "06/04/2016",
+        thumb: protestizando6Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_6_PATH}`
+        },
+    {
+        name: "PROTESTIZANDO #7",
+        releasedAt: "29/04/2016",
+        thumb: protestizando7Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_7_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #8",
+        releasedAt: "30/05/2016",
+        thumb: protestizando8Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_8_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #9",
+        releasedAt: "11/07/2016",
+        thumb: protestizando9Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_9_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #10",
+        releasedAt: "05/09/2016",
+        thumb: protestizando10Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_10_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #11",
+        releasedAt: "06/10/2016",
+        thumb: protestizando11Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_11_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #12",
+        releasedAt: "11/11/2016",
+        thumb: protestizando12Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_12_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #13",
+        releasedAt: "12/12/2016",
+        thumb: protestizando13Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_13_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #14",
+        releasedAt: "28/12/2016",
+        thumb: protestizando14Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_14_PATH}`
+        },
+    {
+        name: "PROTESTIZANDO #15",
+        releasedAt: "06/02/17",
+        thumb: protestizando15Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_15_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #16",
+        releasedAt: "15/03/17",
+        thumb: protestizando16Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_16_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #17",
+        releasedAt: "02/04/2017",
+        thumb: protestizando17Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_17_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #18",
+        releasedAt: "07/06/2017",
+        thumb: protestizando18Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_18_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #19",
+        releasedAt: "17/07/2017",
+        thumb: protestizando19Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_19_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #20",
+        releasedAt: "01/08/2017",
+        thumb: protestizando20Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_20_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #21",
+        releasedAt: "02/09/2017",
+        thumb: protestizando21Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_21_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #22",
+        releasedAt: "07/10/2017",
+        thumb: protestizando22Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_22_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #23",
+        releasedAt: "02/11/2017",
+        thumb: protestizando23Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_23_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #24",
+        releasedAt: "19/12/2017",
+        thumb: protestizando24Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_24_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #25",
+        releasedAt: "20/01/2018",
+        thumb: protestizando25Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_25_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #26",
+        releasedAt: "03/03/2018",
+        thumb: protestizando26Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_26_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #27",
+        releasedAt: "03/05/2018",
+        thumb: protestizando27Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_27_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #28",
+        releasedAt: "26/07/2018",
+        thumb: protestizando28Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_28_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #29",
+        releasedAt: "13/12/2018",
+        thumb: protestizando29Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_29_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #30",
+        releasedAt: "06/07/2019",
+        thumb: protestizando30Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_30_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #31",
+        releasedAt: "18/12/2019",
+        thumb: protestizando31Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_31_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #32",
+        releasedAt: "01/08/2020",
+        thumb: protestizando32Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_32_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #33",
+        releasedAt: "28/11/2021",
+        thumb: protestizando33Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_33_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #34",
+        releasedAt: "26/12/2021",
+        thumb: protestizando34Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_34_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #35",
+        releasedAt: "31/07/2022",
+        thumb: protestizando35Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_35_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #36",
+        releasedAt: "15/10/2023",
+        thumb: protestizando36Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_36_PATH}`
+    },
+    {
+        name: "PROTESTIZANDO #37",
+        releasedAt: "12/07/2024",
+        thumb: protestizando37Thumb,
+        path: `${APP_BASE_URL}${ZINE_PATH}${PROTESTIZANDO_37_PATH}`
+    }
+]
+
+export function getZines() {
+    return zines;
+}

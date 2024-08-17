962f637ebd06ab127902c58ff3f7324ec7dba67f
Daniel Brito <daniell-henrique@hotmail.com>
2024-08-17 01:15:17 +0000

    refactor: adjust illustration style

--- src/components/PublicationItem/PublicationItem.jsx
+++ src/components/PublicationItem/PublicationItem.jsx
@@ -5,11 +5,13 @@ import "./PublicationItem.css";
 function PublicationItem({ publication }) {
   return (
     <div className="wrapper">
-      <div className="w-full h-full">
+      <div className="w-full h-full bg-gray-100">
         <Link to={publication.path}>
           <img
             src={publication.thumb}
             className="w-full h-full object-cover thumb relative z-0 transition-all duration-300 hover:scale-105 hover:shadow-md hover:shadow-gray-400"
+            width={"339px"}
+            height={"480px"}
           />
         </Link>
       </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Button from "../../components/Button/Button";
-import Illustration from "../../components/Illustration/Illustration";
+import Logo from "../../components/Logo/Logo";
 import "./Home.css";
 import { APP_BASE_URL, ZINE_PATH, BOOKS_PATH } from "../../constants/Routes";
 import ButtonsWrapper from "../../components/ButtonsWrapper/ButtonsWrapper";
@@ -7,7 +7,7 @@ import ButtonsWrapper from "../../components/ButtonsWrapper/ButtonsWrapper";
 function Home() {
   return (
     <div id="wrapper">
-      <Illustration />
+      <Logo />
       <ButtonsWrapper>
         <Button url={`${APP_BASE_URL}${ZINE_PATH}`} text={"Zines"} />
         <Button url={`${APP_BASE_URL}${BOOKS_PATH}`} text={"Livros"} />

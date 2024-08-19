9ee9e918465e39d76a2506e101c404d2969f4248
Daniel Brito <daniell-henrique@hotmail.com>
2024-08-19 23:00:31 +0000

    feat: add link to personal website

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import poemixLogo from "../../assets/homepage/poemix_logo.webp";
 import "./Logo.css";
 
 function Logo() {
   return (
     <>
-      <img src={poemixLogo} id="logo" alt="Poemix logo" width={"739px"} height={"263px"} />
+      <Link to={"https://danielbrito.github.io/projetos/#poemix"} target="_blank" rel="noopener noreferrer">
+        <img src={poemixLogo} id="logo" alt="Poemix logo" width={"739px"} height={"263px"} />
+      </Link>
     </>
   );
 }

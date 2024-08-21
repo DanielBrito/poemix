d137e7f2477a063cfbe5a8991dcfd7bc70f734b4
Daniel Brito <daniell-henrique@hotmail.com>
2024-08-21 22:55:13 +0000

    refactor: rename class

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import "./Books.css";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import PublicationList from "../../components/PublicationList/PublicationList";
-import { getBooks } from "../../data/BooksInfo";
+import { getBooks } from "../../data/Books";
 
 function Books() {
   const location = useLocation();

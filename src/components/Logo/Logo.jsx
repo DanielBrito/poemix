dc6ec84093752bb0bbc1392c7992ddbd73de326d
Daniel Brito <daniell-henrique@hotmail.com>
2024-08-21 00:05:21 +0000

    feat: add fade in transition

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,11 +4,11 @@ import "./Logo.css";
 
 function Logo() {
   return (
-    <>
+    <div width={"739px"} height={"263px"} className="max-w-[600px]">
       <Link to={"https://danielbrito.github.io/projetos/#poemix"} target="_blank" rel="noopener noreferrer">
         <img src={poemixLogo} id="logo" alt="Poemix logo" width={"739px"} height={"263px"} />
       </Link>
-    </>
+    </div>
   );
 }
 
